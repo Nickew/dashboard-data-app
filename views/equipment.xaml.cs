0f60586236b6617564bf4d0592dc9ba1eaba487f
Nickew <Extr1m7777@gmail.com>
2018-06-12 16:50:52 +0300

    Equipment entry window

--- views/equipment.xaml.cs
+++ views/equipment.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -30,12 +31,14 @@ namespace intra_app.views
         {
             InitializeComponent();
 
+            appendDock.Height = 55;
+
             loadTable();
         }
 
         private void loadTable()
         {
-            packages.mysql.mysqlConnection mySqlConnection = new packages.mysql.mysqlConnection("SELECT id, name as 'Назва обладнання', divisions_id as 'Відділ' FROM", databaseTable, this.dataGrid);
+            packages.mysql.mysqlConnection mySqlConnection = new packages.mysql.mysqlConnection("SELECT id, name as 'Назва обладнання', divisions_id as 'Відділ', description, technical_data FROM", databaseTable, this.dataGrid);
         }
 
         private void loadRowData(string index)
@@ -91,6 +94,14 @@ namespace intra_app.views
         private void dataGrid_AutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
         {
             if (e.Column.Header.ToString() == "id")
+            {
+                e.Column.MaxWidth = 0;
+            }
+            if (e.Column.Header.ToString() == "description")
+            {
+                e.Column.MaxWidth = 0;
+            }
+            if (e.Column.Header.ToString() == "technical_data")
             {
                 e.Column.MaxWidth = 0;
             }
@@ -255,7 +266,7 @@ namespace intra_app.views
             buttonAdd.Visibility = Visibility.Visible;
             buttonEdit.Visibility = Visibility.Hidden;
 
-            appendDock.Height = 100;
+            appendDock.Height = 290;
         }
 
         private void buttonDeleteEntry_Click(object sender, RoutedEventArgs e)
@@ -297,8 +308,97 @@ namespace intra_app.views
                 {
                     MessageBox.Show("Оберіть запис для редагування", "Selection Error", MessageBoxButton.OK, MessageBoxImage.Information);
                 }
-
             }
+        }
+
+        private void dataGrid_DoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var title = (dataGrid.SelectedCells[1].Column.GetCellContent(dataGrid.SelectedCells[1].Item) as TextBlock).Text;
+            var division = (dataGrid.SelectedCells[2].Column.GetCellContent(dataGrid.SelectedCells[2].Item) as TextBlock).Text;
+            var description = (dataGrid.SelectedCells[3].Column.GetCellContent(dataGrid.SelectedCells[3].Item) as TextBlock).Text;
+            var techData = (dataGrid.SelectedCells[4].Column.GetCellContent(dataGrid.SelectedCells[4].Item) as TextBlock).Text;
+            
+            #region
+            /*
+            TextBlock tbTitle = new TextBlock();
+            TextBlock tbDivision = new TextBlock();
+            TextBox tbDescription = new TextBox();
+            TextBox tbTechData = new TextBox();
+
+            Label lbDivision = new Label();
+            Label lbDescription = new Label();
+            Label lbTechData = new Label();
+            Grid grid = new Grid();
+
+            Window wnd = new Window();
+
+            #region Create Window programmatically
+            // Grid settings
+            grid.RowDefinitions.Add(new RowDefinition());
+            grid.RowDefinitions.Add(new RowDefinition());
+            grid.RowDefinitions.Add(new RowDefinition());
+            grid.RowDefinitions.Add(new RowDefinition());
+            grid.RowDefinitions.Add(new RowDefinition());
+            grid.RowDefinitions.Add(new RowDefinition());
+            grid.RowDefinitions[0].Height = new GridLength(50);
+            grid.RowDefinitions[1].Height = new GridLength(40);
+            grid.RowDefinitions[2].Height = new GridLength(40);
+            grid.RowDefinitions[4].Height = new GridLength(40);
+            // Window settings
+            wnd.Height = 580;
+            wnd.Width = 760;
+            wnd.Title = title;
+            // TextBlocks
+            tbTitle.Text = title;
+            tbTitle.Margin = new Thickness(10, 10, 10, 0);
+            tbTitle.FontSize = 16;
+            tbTitle.FontWeight = FontWeights.Bold;
+            tbDivision.Text = division;
+            tbDivision.FontWeight = FontWeights.Bold;
+            tbDivision.Margin = new Thickness(60, 10, 10, 10);
+            tbDescription.Text = description;
+            tbDescription.Margin = new Thickness(20, 0, 10, 0);
+            tbDescription.IsReadOnly = true;
+            tbTechData.Text = techData;
+            tbTechData.Margin = new Thickness(20, 0, 10, 10);
+            tbTechData.IsReadOnly = true;
+            // Labels
+            lbDivision.Content = "Відділ: ";
+            lbDivision.Margin = new Thickness(10, 5, 0, 0);
+            lbDescription.Content = "Опис:";
+            lbDescription.Margin = new Thickness(10, 5, 10, 5);
+            lbTechData.Content = "Технічні дані:";
+            lbTechData.Margin = new Thickness(10, 5, 10, 5);
+            // fill grid, window with content
+            grid.Children.Add(tbTitle);
+            grid.Children.Add(tbDivision);
+            grid.Children.Add(tbDescription);
+            grid.Children.Add(tbTechData);
+            grid.Children.Add(lbDivision);
+            grid.Children.Add(lbDescription);
+            grid.Children.Add(lbTechData);
+            Grid.SetRow(tbTitle, 0);
+            Grid.SetRow(tbDivision, 1);
+            Grid.SetRow(tbDescription, 3);
+            Grid.SetRow(tbTechData, 5);
+            Grid.SetRow(lbDivision, 1);
+            Grid.SetRow(lbDescription, 2);
+            Grid.SetRow(lbTechData, 4);
+            wnd.Content = grid;
+            #endregion
+
+            wnd.ShowDialog();
+             * */
+            #endregion
+
+            windows.equipmentPage window = new windows.equipmentPage();
+            window.Title = title;
+            window.lbTitle.Content = title;
+            window.tbDivision.Text = division;
+            window.tbDesc.Text = description;
+            window.tbTechData.Text = techData;
+            window.ShowDialog();
+            
         }
     }
 }
